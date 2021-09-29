2437b4b82819c0051500887004c683f414c9be91
Jade Neff <njade79162@gmail.com>
2021-09-28 21:24:04 -0400

    Add project files.

---
+++ FacebookProfileConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using _9_28_2021_Challenge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookProfileConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FacebookProfile facebookProfile = new FacebookProfile("Jade", "Neff", new DateTime(2003, 09, 17), "None");
+
+            string fullName = facebookProfile.getFullName();
+            Console.WriteLine(fullName);
+
+            Console.ReadKey();
+
+            string profile = facebookProfile.getProfile();
+            Console.WriteLine(profile);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Facebook_Profile/FacebookProfile.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_28_2021_Challenge
+{
+    public class FacebookProfile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+        public int AgeInYears
+        {
+            get
+            {
+                TimeSpan ageSpan = DateTime.Now - DateOfBirth;
+                double totalAgeInYears = ageSpan.TotalDays / 365.25;
+                return Convert.ToInt32(Math.Floor(totalAgeInYears));
+            }
+        }
+        public string Work { get; set; }
+
+        public FacebookProfile()
+        {
+
+        }
+
+        public FacebookProfile(string firstName, string lastName, DateTime dateOfBirth, string work)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Work = work;
+        }
+
+        public string getFullName()
+        {
+            return FullName;
+        }
+
+        public string getProfile()
+        {
+            return $"First Name: {FirstName}, Last Name: {LastName}, Full Name: {FullName}, Age: {AgeInYears}, Work: {Work}";
+        }
+    }
+}
